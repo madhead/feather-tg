55d00ac5bb5e9d68cbc4f4b5b0ed6ca2bdb3093a
madhead <siarhei.krukau@gmail.com>
2024-03-29 23:48:33 +0100

    Render icons into 100×100 PNGs for Telegram static emoji pack

--- bin/build.sh
+++ bin/build.sh
@@ -17,6 +17,11 @@ mkdir dist/icons
 # Build SVG icons
 npx babel-node bin/build-svgs.js
 
+# Build PNG icons
+for svg in dist/icons/*.svg; do
+    inkscape -w 100 -h 100 $svg -o "dist/icons/$(basename $svg .svg).png"
+done
+
 # Build JavaScript library
 npx webpack --output-filename feather.js --mode development
 npx webpack --output-filename feather.min.js --mode production
